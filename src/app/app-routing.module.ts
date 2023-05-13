9cefe9ca100eca8db2e86300bb0c2cdacd358afd
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-05-13 22:57:24 +0700

    update tailwindcss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+    // { path: "", component: }, homepage
+    // { path: "**", component: }, homepage
+    // {path : "product/:id" , component : }, deail product
+    // {path : "signin" , component : },
+    // {path : "signup" , component : },
+    // {path : "" , component : },
+    // {path : "" , component : },
+    // {path : "" , component : },
+    // {path : "" , component : },
+];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
