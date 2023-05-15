c17573a44cb94aaa021d5cbf8294a391b397e16a
VuHuuQuan1692003 <vuhuuquan169@gmail.com>
2023-05-15 22:25:32 +0700

    header+detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
