fd9137176a429eec48a0933e511725840510606b
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-06-10 07:28:03 +0700

    validate signin

--- src/app/pages/client/sign-in/sign-in.component.ts
+++ src/app/pages/client/sign-in/sign-in.component.ts
@@ -19,8 +19,8 @@ export class SignInComponent {
   submited = false
   errer = false
   signinform = new FormGroup({
-    email : new FormControl("",[Validators.required ]),
-    password : new FormControl("",[Validators.required ]),
+    email : new FormControl("",[Validators.required ,Validators.email]),
+    password : new FormControl("",[Validators.required,Validators.minLength(6) ]),
   })
   get email() {
     return this.signinform.get("email")
@@ -28,14 +28,51 @@ export class SignInComponent {
   get password() {
     return this.signinform.get("password")
   }
+  getpwErrorMessage() {
+    if (this.password) {
+      if (this.password.hasError('required') || "") {
+        return 'password is required';
+      }     
+      if (this.password?.value!.length < 6) {
+        return "password must not be less than 6"
+      }
+    }
+    return false
+  }
+  getemailErrorMessage() {
+    if (this.email) {
+     if (this.email.hasError('required') || "") {
+       return 'Email is required';
+     }
+     if (this.email.hasError('email')) {
+       return 'Email is Invalid';
+      }
+      if (this.email.hasError('unique')) {
+       return 'Email is registered';
+     }
+     }    
+   throw new Error('Lỗi không rõ nguyên nhân');
+ 
+  }
 
   onSubmit() { 
     if (this.signinform.valid) {
       console.log('Login submitted:', this.signinform.value);
       this.submited = true
       // Goi API
-      this.UserService.signin(this.email?.value || "", this.password?.value || "")
-      
+     try {
+    const er =  this.UserService.signin(this.email?.value || "", this.password?.value || "")
+      console.log( this.UserService.signin(this.email?.value || "", this.password?.value || "")
+       );
+       if (typeof er === "undefined") {
+         this.errer = true
+         console.log("looix");
+         this.submited = true
+       }
+     } catch (error) {
+      this.submited = true
+        this.errer = false
+      }
         
     } else {
       this.submited = true
