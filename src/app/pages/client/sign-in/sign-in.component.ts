034c0e05269dcdc4d1560963a4ad7deec7cf9697
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-06-10 07:31:17 +0700

    FIX signin

--- src/app/pages/client/sign-in/sign-in.component.ts
+++ src/app/pages/client/sign-in/sign-in.component.ts
@@ -12,15 +12,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
- 
+
   constructor(
     private UserService: UserService,
-  ){}
+  ) { }
   submited = false
   errer = false
   signinform = new FormGroup({
-    email : new FormControl("",[Validators.required ,Validators.email]),
-    password : new FormControl("",[Validators.required,Validators.minLength(6) ]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
   })
   get email() {
     return this.signinform.get("email")
@@ -32,7 +32,7 @@ export class SignInComponent {
     if (this.password) {
       if (this.password.hasError('required') || "") {
         return 'password is required';
-      }     
+      }
       if (this.password?.value!.length < 6) {
         return "password must not be less than 6"
       }
@@ -41,47 +41,42 @@ export class SignInComponent {
   }
   getemailErrorMessage() {
     if (this.email) {
-     if (this.email.hasError('required') || "") {
-       return 'Email is required';
-     }
-     if (this.email.hasError('email')) {
-       return 'Email is Invalid';
+      if (this.email.hasError('required') || "") {
+        return 'Email is required';
+      }
+      if (this.email.hasError('email')) {
+        return 'Email is Invalid';
       }
       if (this.email.hasError('unique')) {
-       return 'Email is registered';
-     }
-     }    
-   throw new Error('Lỗi không rõ nguyên nhân');
- 
-  }
+        return 'Email is registered';
+      }
+    }
+    throw new Error('Lỗi không rõ nguyên nhân');
 
-  onSubmit() { 
+  }
+  onSubmit() {
+    this.submited = true
     if (this.signinform.valid) {
       console.log('Login submitted:', this.signinform.value);
-      this.submited = true
       // Goi API
-     try {
-    const er =  this.UserService.signin(this.email?.value || "", this.password?.value || "")
-      console.log( this.UserService.signin(this.email?.value || "", this.password?.value || "")
-       );
-       if (typeof er === "undefined") {
-         this.errer = true
-         console.log("looix");
-         this.submited = true
-       }
-     } catch (error) {
-      this.submited = true
+      try {
+        const er = this.UserService.signin(this.email?.value || "", this.password?.value || "")
+        console.log(this.UserService.signin(this.email?.value || "", this.password?.value || "")
+        );
+        if (typeof er === "undefined") {
+          // this.errer = true
+          console.log("looix");
+        }
+      } catch (error) {
         this.errer = false
       }
-        
     } else {
-      this.submited = true
       this.errer = false
       console.log("invalid");
     }
   }
 
-   gotoTop() {
+  gotoTop() {
     window.scroll({
       top: 0,
       left: 0,
